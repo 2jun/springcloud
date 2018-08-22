bfcbbbd4d8e757eea822e51d47e0c5301816e4dd
aj <724755391@qq.com>
2018-08-22 22:25:55 +0800

    提交服务

--- src/main/java/com/leyou/userservicedemo/UserServiceDemoApplication.java
+++ src/main/java/com/leyou/userservicedemo/UserServiceDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@enable
 public class UserServiceDemoApplication {
 
 	public static void main(String[] args) {
